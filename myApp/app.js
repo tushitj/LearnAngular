cbebf1eb8536952849ba976ba25101cf18a3cc22
Tushit Jain <tushitjain@tushits-air.attlocal.net>
2017-06-01 22:12:29 -0700

    array of products with show and hide directives

--- myApp/app.js
+++ myApp/app.js
@@ -5,15 +5,33 @@
     var app = angular.module('store' ,[]);
 
     app.controller('StoreController',function(){
-    this.product = gem;
+    this.product = gems;
     });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 29.5,
-        description: "this is the item1",
+    var gems = [
+        {
+            name: 'Product1',
+            price: 100,
+            description: "This is product 1 and is not available to add to cart is not soldOut",
+            canPurchase: false,
+            soldOut: false,
+        },
+        {
+            name: 'Product2',
+            price: 200,
+            description: "This is product 2 and is available to add to cart and is not sold out",
+            canPurchase: true,
+            soldOut: false,
+        },
+        {
+            name: 'Product3',
+            price: 300,
+            description: "This is product 3 and is not available to add to cart and is sold out",
+            canPurchase: true,
+            soldOut: true,
 
-    }
+        },
+    ];
 
 
 })();
